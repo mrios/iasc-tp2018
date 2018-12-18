a4ec296c94f646afb9f27c59e90149a6c7fac6da
EvarMarro <jose.evaristo.marro@gmail.com>
2018-12-17 22:58:44 -0300

    multiples workers sobre la misma cola

--- producer/app/producer.js
+++ producer/app/producer.js
@@ -1,14 +1,28 @@
 // producer.js
 const socket = require(`zeromq`).socket(`push`);
+var counter = 0;
 const address = process.env.ZMQ_PRODUCER_ADDRESS || `tcp://*:3000`;
 const interval = process.env.ZMQ_PRODUCER_INTERVAL || 2000;
 
-console.log(`Producer bound to address: ${address}`);
-socket.bindSync(address);
+socket.bind(address, function(error){
+    if (error) {
+        logToConsole("Failed to bind socket: " + error.message);
+        process.exit(0);
+    }
+    else {
+        logToConsole("Server listening on port 9998");
 
-const sendMessage = function () {
-	console.log('sending work');
-  	socket.send('some work');
-};
+        // Increment the counter and send the value to the clients every second.
+        setInterval(function () { sendMessage(counter++); }, 1000);
+    }
+});
 
-setInterval(sendMessage, interval);
+function logToConsole (message) {
+    console.log("[" + new Date().toLocaleTimeString() + "] " + message);
+}
+
+function sendMessage (message) {
+    logToConsole("Sending: " + message);
+    socket.send(message);
+}
+
--- worker/app/worker.js
+++ worker/app/worker.js
@@ -2,11 +2,16 @@
 const socket = require(`zeromq`).socket(`pull`);
 const address = process.env.ZMQ_WORKER_ADDRESS || `tcp://127.0.0.1:3000`;
 
-console.log(`Connecting to ${address}`);
+function logToConsole (message) {
+    console.log("[" + new Date().toLocaleTimeString() + "] " + message);
+}
 
-socket.connect(address);
-console.log(`Subscriber connected to address: ${address}`);
-
-socket.on(`message`, function (msg) {
-	console.log(`Message received: ${msg}`);
+// Add a callback for the event that is invoked when we receive a message.
+socket.on("message", function (message) {
+    // Convert the message into a string and log to the console.
+    logToConsole("Received message: " + message.toString("utf8"));
 });
+
+// Connect to the server instance.
+logToConsole("Connected to address: " + address);
+socket.connect(address);
