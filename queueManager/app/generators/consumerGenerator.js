97a10e18ff8b3b09ea226ddcdabb57e5a409886a
mrios <riosmatias.zarate@gmail.com>
2018-12-24 14:20:34 -0300

    Refactor generators, add extra channels for consumers and cli-colors for both

--- queueManager/app/generators/consumerGenerator.js
+++ queueManager/app/generators/consumerGenerator.js
@@ -1,42 +1,74 @@
 const zmq = require('zeromq');
+const chalk = require('chalk');
 
 class ConsumerGenerator {
     constructor() {
     }
 
     init(config) {
+        let cliColor = '';
+        this.setCliColor(config.cliColor);
         for (let index = 0; index < config.numConsumers; index++) {
-            this.createConsumers(config, index);
+            this.createConsumer(config, index);
         }
     }
 
-    createConsumers(configConsumers, index) {
+    setCliColor(color) {
+        this.cliColor = color;
+    }
+
+    getCliColor(color) {
+        return this.cliColor;
+    }
+
+    createConsumer(configConsumers, index) {
         const consumer = zmq.socket(configConsumers.mode);
         const ackPubSock = zmq.socket('pub');
+        const reqSyncSock = zmq.socket('req');
         consumer.identity = `Consumer-${index}`;
         
         if(configConsumers.mode === 'sub') {
             const topic = this.getRandomTopic(configConsumers.topics);
             consumer.connect(configConsumers.host);
             consumer.subscribe(topic);
-			ackPubSock.connect(configConsumers.ackHost);
-            consumer.on('message', function(topic, msg) {
-				var jobId = msg.toString().substr(0,8);
-				ackPubSock.send([topic, jobId + consumer.identity]);
-                console.log(`Consumer-${index}, Received Topic: ${topic}, msg: ${msg}`);
+
+            // Parralel comunication for sync
+            reqSyncSock.connect(configConsumers.reqHost);
+            reqSyncSock.send([consumer.identity, topic]);
+
+            // ACK pub socker
+            ackPubSock.connect(configConsumers.ackHost);
+            
+            consumer.on('message', (topic, msg) => {
+				const jobId = msg.toString().substr(0,8);
+                this.log(`Sending ACK topic: ${topic}, jobId: ${jobId} consumerId: ${consumer.identity}`);
+                ackPubSock.send([topic, jobId, consumer.identity]);
+                this.log(`DONE! ${consumer.identity}, Received Topic: ${topic} and msg: ${msg}`);
             });
         }
         else {
             consumer.connect(configConsumers.host);
-            consumer.on('message', function(msg) {
-                console.log(`Consumer-${index}, msg: ${msg}`)
+            consumer.on('message', (msg) => {
+                this.log(`Consumer-${index}, msg: ${msg}`);
             });
         }
+
+        // Handling Interrupt Signals
+        process.on('SIGINT', () => {
+            consumer.close();
+            //ackPubSock.close();
+            reqSyncSock.close();
+            process.exit();
+        });
 
     }
 
     getRandomTopic(topics) {
         return topics[Math.floor(Math.random()*topics.length)]
+    }
+
+    log(msg) {
+        console.log(chalk.hex(this.getCliColor())(msg));
     }
 }
 
--- queueManager/app/generators/producerGenerator.js
+++ queueManager/app/generators/producerGenerator.js
@@ -1,40 +1,61 @@
 const zmq = require('zeromq');
+const chalk = require('chalk');
 
 class ProducerGenerator {
     constructor() {
     }
 
     init(config) {
+        let cliColor = '';
+        this.setCliColor(config.cliColor);
         for (let index = 0; index < config.numProducers; index++) {
-            this.createProducers(config, index);
+            this.createProducer(config, index);
         }
     }
 
-    createProducers(configProducers, index) {
+    setCliColor(color) {
+        this.cliColor = color;
+    }
+
+    getCliColor(color) {
+        return this.cliColor;
+    }
+
+    createProducer(configProducers, index) {
         const producer = zmq.socket(configProducers.mode);
         producer.identity = `Producer-${index}`;
         
         if(configProducers.mode === 'pub') {
             producer.connect(configProducers.host);
-            console.log(`${producer.identity} bound to ${configProducers.host}!`);
+            this.log(`${producer.identity} bound to ${configProducers.host}!`);
             setInterval(() => {
                 const topic = this.getRandomTopic(configProducers.topics)
-                , msg = `The magical number is: ${Math.random()*1000}`;
-                console.log(producer.identity + ': sent ' + topic + ' ' + msg);
+                , msg = `The magic number is: ${Math.random()*1000}`;
+                this.log(`${producer.identity}: sent topic: ${topic}, msg: ${msg}`);
                 producer.send([topic, msg]);
             }, configProducers.interval);
         }
         else {
             producer.connect(configProducers.host);
-            producer.on('message', function(msg) {
-                console.log(`Producer-${index}, msg: ${msg}`)
+            producer.on('message', (msg) => {
+                this.log(`Producer-${index}, msg: ${msg}`);
             });
         }
+
+        // Handling Interrupt Signals
+        process.on('SIGINT', () => {
+            producer.close();
+            process.exit();
+        });
 
     }
 
     getRandomTopic(topics) {
         return topics[Math.floor(Math.random()*topics.length)]
+    }
+
+    log(msg) {
+        console.log(chalk.hex(this.getCliColor())(msg));
     }
 }
 
