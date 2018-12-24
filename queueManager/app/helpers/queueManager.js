8b9060c17427cc795c9ab1622c7c347811218379
mrios <riosmatias.zarate@gmail.com>
2018-12-24 14:23:54 -0300

    Add extra channels to sync and register consumers, cli-colors and queue methods: remove, process

--- queueManager/app/helpers/queueManager.js
+++ queueManager/app/helpers/queueManager.js
@@ -1,17 +1,28 @@
 const Queue = require('bee-queue');
 const _ = require('lodash');
 const zmq = require('zeromq');
+const chalk = require('chalk');
 
 class QueueManager {
     
     constructor() {}
 
     init(config) {
+        let cliColor = ''
         let queueStore = [];
         let queueConfig = {};
         this.setQueuesStore([]);
+        this.setCliColor(config.queue.proxy.cliColor);
         this.initProxy(config.queue.proxy);
         this.setQueueConfig(config.cluster.redis);
+    }
+
+    setCliColor(color) {
+        this.cliColor = color;
+    }
+
+    getCliColor(color) {
+        return this.cliColor;
     }
 
     // Private methods
@@ -28,12 +39,12 @@ class QueueManager {
         if(configProxy.mode === 'xsub/xpub') {
 
             // Create de xsub socket, identity and connect it
-            var xsubSock = zmq.socket('xsub');
+            const xsubSock = zmq.socket('xsub');
             xsubSock.identity = 'subscriber' + process.pid;
             xsubSock.bindSync(configProxy.subListener);
 
-            // Create de xpub socket, identity and connect it
-            var xpubSock = zmq.socket('xpub');
+            // Create xpub socket, identity and connect
+            const xpubSock = zmq.socket('xpub');
             xpubSock.identity = 'publisher' + process.pid;
             // Set Hight water mark: HWM,
             // This tells us how many messages we want ZeroMQ to buffer in RAM before blocking the 'pushing' socket.
@@ -42,36 +53,54 @@ class QueueManager {
             // This allow us enable/disable verbose tracing of commands and activity
             xpubSock.setsockopt(zmq.ZMQ_XPUB_VERBOSE, configProxy.verbose);
             xpubSock.bindSync(configProxy.pubListener);
-			
-			var ackSubSock = zmq.socket('sub');
-			ackSubSock.subscribe('');
-			ackSubSock.bind(configProxy.ackHost);
+
+            // Create rep sync socket, register consumer when sync
+            const repSyncSock = zmq.socket('router');
+            repSyncSock.bind(configProxy.repHost);
+            
+            repSyncSock.on('message', (generatedId, emptyDelimiter, consumer, topic) => {
+                this.registerConsumer({
+                    genId: generatedId.toString(),
+                    consumer: consumer.toString('utf8'),
+                    topic: topic.toString('utf8'),
+                });
+            });
 			
             // Set Listener for xsub
             xsubSock.on('message', (topic, msg) => {
                 // Store messages as jobs in the BeeQueue by topic, 1 topic <---> 1 beeQueue
                 let beeQFound = this.findOneQueueBy({field: 'name', value: topic});
                 if(!beeQFound) {
-                    beeQFound = this.createQueue({name: topic});
+                    beeQFound = this.createQueue({name: topic.toString('utf8')});
                 }
                 this.createJob(beeQFound, msg.toString('utf8'), topic, xpubSock);
             });
 
             // Set Listener for xpub
-            xpubSock.on('message', (data, bla) => {                
-                var type = data[0]===0 ? 'unsubscribe' : 'subscribe';
-                var topic = data.slice(1).toString();
-                console.log(`Type: ${type} : ${topic}`);
-                xsubSock.send(data);
+            xpubSock.on('message', (data, bla) => { 
+                const type = data[0]===0 ? 'unsubscribe' : 'subscribe';
+                const topic = data.slice(1).toString();
+                this.log(`Type: ${type}, topic: ${topic}`);
+                
+                // Add delay to store jobs
+                setTimeout( ()=> {
+                    xsubSock.send(data);
+                }, configProxy.timeoutFordwarding)
                 //TODO quitar elemento de la cola
-            }); 
+            });
+
+            // Create ack sub socket, subscribe to all topics and connect
+			const ackSubSock = zmq.socket('sub');
+			ackSubSock.subscribe('');
+            ackSubSock.bind(configProxy.ackHost);
         
 			// Set Listener for ACK Receiver
-			ackSubSock.on('message', function(topic, msg) {
-				var jobId = msg.toString().substr(0,8);
-                var consumerId = msg.toString().substr(8);
-                console.log('jobId', jobId)
-                console.log('consumerId', consumerId)
+			ackSubSock.on('message', (topic, jobId, consumer) => {
+				this.registerACK({
+                    jobId: jobId.toString(),
+                    consumer: consumer.toString('utf8'),
+                    topic: topic.toString('utf8')
+                })
             });
 		}
         else {
@@ -144,22 +173,30 @@ class QueueManager {
     }
     
     removeQueue(name) {
-        this.removeQ(name);
         const found = this.findOneQueueBy({field: 'name', value: name});
-        if(!_.isObject(found)) {
-            return `Queue with name: ${name} deleted succesfully`
-        }
-        else {
-            return `Queue with name: ${name} not found`
-        }
-        
+        found.beeQ.destroy((err) => {
+            if (!err) {
+                console.log('Queue was destroyed');
+            }
+            this.removeQ(name);
+            const foundAfterRemove = this.findOneQueueBy({field: 'name', value: name});
+            if(!_.isObject(foundAfterRemove)) {
+                return `Queue with name: ${name} deleted succesfully`
+            }
+            else {
+                return `Queue with name: ${name} not found`
+            }
+        });
     }
 
 	padJobId(jobId){
-		var result = "00000000" + jobId;
-		return result.substr(result.length - 8);
+		return _.padStart(jobId, 8, '0');
     }
-    
+
+    unpadJobId(jobId){
+		return jobId.substr(-4);
+    }
+
     createJob(beeQ, msg, topic, xpubSock) {
         const job = beeQ.createJob({msg: msg});
         job
@@ -168,10 +205,70 @@ class QueueManager {
             .save()
             .then((job) => {
                 // job enqueued, job.id populated
-				var msgWithId =  this.padJobId(job.id) + msg;
+				const msgWithId =  this.padJobId(job.id) + msg;
 				xpubSock.send([topic, msgWithId]);
             });
+
+        job.on('progress', (progress) => {
+            this.log(`Job ${job.id} reported progress: page ${progress.page} / ${progress.totalPages}`);
+        });
     }
+
+    registerConsumer({genId, consumer, topic}) {
+        // TODO Register consumer for given topic in beeQ-manager
+        this.log(`Consumer Registered`, [genId, consumer, topic]);
+    }
+
+    registerACK({jobId, consumer, topic}) {
+        this.log(`ACK Registered`, [jobId, consumer, topic]);
+        let found = this.findOneQueueBy({field: 'name', value: topic});
+
+        if(found) {
+            const data = {
+                jobId: this.unpadJobId(jobId), 
+                consumer: consumer, 
+                beeQ: found.beeQ
+            }
+
+            this.markConsumedBy(data);
+            if(this.canRemove(data)) {
+                // TODO: process after check, this operation can be executed only once
+                // data.beeQ.process(async (job) => {
+                //     this.log(`Processing job ${job.id}, msg: ${job.data.msg}`);
+                //     return job.data.msg;
+                // });
+                this.removeJob(data);
+            }
+        }
+        else {
+            this.log(`BeeQueue for topic: ${topic} not found!`);
+        }
+
+    }
+
+    markConsumedBy({jobId, consumer, beeQ}) {
+        // TODO: mark jobId consumed by consumer in beeQ-manager
+        this.log(`The job: ${jobId} was consumed by: ${consumer}`);
+    }
+
+    canRemove({jobId, consumer, beeQ}) {
+        // TODO: validate vs consumers by topic register
+        return true;
+    }
+
+    removeJob({jobId, consumer, beeQ}) {
+        beeQ.removeJob(jobId, (err) => {
+            if (!err) {
+              this.log(`Job ${jobId} was removed`, '#FF0000');
+            }
+        });
+    }
+
+    log(msg, color = null) {
+        const hexa = _.isString(color) ? color : this.getCliColor();
+        console.log(chalk.hex(hexa)(msg));
+    }
+
 }
 
 module.exports = new QueueManager();
